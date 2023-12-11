867419c5523eb465fe4572560e2ce7427b2e57c0
Rick South <rick@natamus.com>
2023-12-11 23:43:39 +0100

    Wool Tweaks source code for 1.18.2.

---
+++ Forge/src/main/java/com/natamus/wooltweaks/forge/events/ForgeWoolClickEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.wooltweaks.forge.events;
+
+import com.natamus.wooltweaks.events.WoolClickEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeWoolClickEvent {
+	@SubscribeEvent
+	public void onWoolClick(PlayerInteractEvent.RightClickBlock e) {
+		WoolClickEvent.onWoolClick(e.getWorld(), e.getPlayer(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+}
