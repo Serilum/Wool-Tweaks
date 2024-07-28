99da5a7d854f6ca83777ce01e7fa48c51eadf63b
Rick South <rick@natamus.com>
2024-07-28 02:35:05 +0200

    (v3.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/wooltweaks/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/wooltweaks/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.wooltweaks;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.wooltweaks.neoforge.events.NeoForgeWoolClickEvent;
 import com.natamus.wooltweaks.util.Reference;
 import net.neoforged.neoforge.common.NeoForge;
@@ -13,6 +14,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
