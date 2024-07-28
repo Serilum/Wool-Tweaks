820096aa49079d175789595d176194e67708cca2
Rick South <rick@natamus.com>
2024-07-28 02:35:01 +0200

    (v3.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/wooltweaks/ModFabric.java
+++ Fabric/src/main/java/com/natamus/wooltweaks/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.wooltweaks;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
 import com.natamus.wooltweaks.events.WoolClickEvent;
 import com.natamus.wooltweaks.util.Reference;
@@ -10,6 +11,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
