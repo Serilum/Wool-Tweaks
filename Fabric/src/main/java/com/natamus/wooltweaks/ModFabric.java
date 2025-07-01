f4345ac29d4ebb2adae47b1e6da6ea167899627d
Rick South <rick@natamus.com>
2025-07-01 09:47:20 +0200

    Wool Tweaks source code for 1.21.7.

---
+++ Fabric/src/main/java/com/natamus/wooltweaks/ModFabric.java
@@ -0,0 +1,35 @@
+package com.natamus.wooltweaks;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
+import com.natamus.wooltweaks.events.WoolClickEvent;
+import com.natamus.wooltweaks.util.Reference;
+import net.fabricmc.api.ModInitializer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveBlockEvents.BLOCK_RIGHT_CLICK.register((world, player, hand, pos, hitVec) -> {
+			return WoolClickEvent.onWoolClick(world, player, hand, pos, hitVec);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
